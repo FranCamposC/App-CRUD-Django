58efe63a70a0c984db1b7281bd51d67542c31481
FranCamposC <fran.antonio2801@gmail.com>
2023-12-29 17:52:25 +0100

    Nuevas funcionalidades

--- libreria/urls.py
+++ libreria/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('nosotros', views.contacto ,name='nosotros'), #accedemos a nosotros, views.
     #(el nombre de la funcion en el views), name= el que queramos normalmente 
     #el mismo que pongamos en el path
-    path('libros', views.libros, name='libros')
+    path('libros', views.libros, name='libros'),
+    path('libros/crear', views.crear,name='crear'),
+    path('libros/editar', views.editar,name='editar'),
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -13,3 +13,9 @@ def contacto(request):
 
 def libros(request):   
     return render(request,'libros/index.html')
+
+def crear(request):   
+    return render(request,'libros/crear.html')
+
+def editar(request):   
+    return render(request,'libros/editar.html')
